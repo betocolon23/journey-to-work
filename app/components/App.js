58623f6cf0e8c9a020e0d9acc41301aa6a357a1d
Beto Colon <betocolon23@gmail.com>
2018-05-16 15:56:57 -0400

    legend edited

--- app/components/App.js
+++ app/components/App.js
@@ -15,16 +15,6 @@ import csvData from './csvData.js';
 //data
 const geoJsonFeature = require('./geoJsonData.json')
 const county_data = require('./county-data')
-// const csvData = require('./csvData.js');
-
-const dataTest =[
-        ['County Code Residence', 'County Name', 'County Code Place of Work', 'County Name', 'Workers in Commuting Flow'],
-        ['72001', 'Adjuntas ', '72001', 'Adjuntas ', '2319'],
-        ['72001', 'Adjuntas ', '72013', 'Arecibo ', '20'],
-        ['72001', 'Adjuntas ', '72021', 'Bayamon ', '13'],
-        ['72001', 'Adjuntas ', '72023', 'Cabo Rojo ', '11'],
-        ['72001', 'Adjuntas ', '72025', 'Caguas ', '6']
-  ];
 
 
 const styles = {
@@ -36,7 +26,7 @@ const styles = {
     },
 };
 
-const prettyLink  = {
+const prettyLink = {
     backgroundColor: '#fffff',
     fontSize: 14,
     fontWeight: 100,
@@ -44,10 +34,12 @@ const prettyLink  = {
     padding: '5 5',
     borderRadius: 1,
     color: '#000'
-  };
+};
 
-  
-  export default class App extends React.Component {
+const county_csv_headers = ['County Name', 'Workers in Commuting Flow'];
+
+
+export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -67,7 +59,13 @@ const prettyLink  = {
         var newSelected = this.state.selectedOption;
         var inbound = document.getElementById('inbound');
         var outbound = document.getElementById('outbound');
-         
+        var intervalBreak;
+        var firstBreak;
+        var secondBreak;
+        var thirdBreak;
+        var fourthBreak;
+        var fifthBreak;
+        var selected_county_csv;
 
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmV0b2NvbG9uMjMiLCJhIjoiY2pmMWNuY2g1MDdtaDJ5bG44aGFoNmdlZCJ9.L_4W1fZnk7hMCwmS71Lg1w', {
             id: 'mapbox.light',
@@ -81,6 +79,14 @@ const prettyLink  = {
             return this._div;
         }
 
+        function getColor(d) {
+            return d > 1000 ? '#994d00' :
+                d > 500 ? '#cc6600' :
+                    d > 100 ? '#ff8000' :
+                        d > 10 ? '#ff9933' :
+                            '#ffbf80';
+        }
+
         info.update = function (props) {
             if (props) {
                 if (outbound.checked) {
@@ -90,12 +96,19 @@ const prettyLink  = {
                         }
                     });
                     county_outbound = county_outbound.filter(Boolean);
-                    console.log(county_outbound);
-
                     this._div.innerHTML = (props ?
                         '<h4>' + props.Municipio + '</h4>'
-                        // county_outbound + '</div>' + '</br>'
                         : 'Click over a county');
+
+                    function csvCountyData() {
+                        var selected_county_csv = county_outbound
+                        console.log(selected_county_csv);
+                        return [
+                            selected_county_csv
+                        ]
+                        console.log(selected_county_csv);
+                    }
+                    csvCountyData();
                 }
                 else {
                     county_inbound = county_data.features.map(function (feature) {
@@ -104,11 +117,19 @@ const prettyLink  = {
                         }
                     });
                     county_inbound = county_inbound.filter(Boolean);
-                    // console.log(county_inbound);
                     this._div.innerHTML = (props ?
                         '<h4>' + props.Municipio + '</h4>'
-                        //  + county_inbound + '</div>' + '</br>'
                         : 'Click over a county');
+
+                    function csvCountyData() {
+                        var selected_county_csv = county_inbound
+                        console.log(selected_county_csv);
+                        return [
+                            selected_county_csv
+                        ]
+                        console.log(selected_county_csv);
+                    }
+                    csvCountyData();
                 }
             }
         };
@@ -125,21 +146,15 @@ const prettyLink  = {
         }
 
         function setJenks(arr) {
-            // ------------ mierda de jenks ------------
             function nl2br(str, is_xhtml) {
                 var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
                 return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
             }
-            
-    
+
             var serie = new geostats();
-            // debugger;
             serie.setSerie(arr);
-
             var intervalAmount = arr.length >= 5 ? 5 : arr.length;
-        
-
-            var a = serie.getClassJenks(intervalAmount);
+            intervalBreak = serie.getClassJenks(intervalAmount);
             var str = '<strong>Classification Method : <\/strong>' + serie.method + " :\n";
             str += '<div class="classes">';
             var ranges = serie.ranges;
@@ -147,25 +162,59 @@ const prettyLink  = {
                 str += ranges[i] + "\n";
             }
             str += '<\/div>';
+
+            // document.write('<p>' + nl2br(serie.info()) + '<\/p>');
+            // document.write('<p>' + nl2br(str) + '<\/p>');
+            // console.log(intervalBreak);
+            firstBreak = intervalBreak[0];
+            secondBreak = intervalBreak[1];
+            thirdBreak = intervalBreak[2];
+            fourthBreak = intervalBreak[3];
+            fifthBreak = intervalBreak[4];
+
+            console.log(ranges);
+            console.log(intervalBreak);
+            // console.log(nl2br(str));
+
+            function getColor(d) {
+                return d > fifthBreak ? '#994d00' :
+                    d > fourthBreak ? '#cc6600' :
+                        d > thirdBreak ? '#ff8000' :
+                            d > secondBreak ? '#ff9933' :
+                                '#ffbf80';
+            }
     
-            document.write('<p>' + nl2br(serie.info()) + '<\/p>');
-            document.write('<p>' + nl2br(str) + '<\/p>');
+            var legend = L.control({ position: 'bottomright' });
+            legend.onAdd = function (map) {
+                var div = L.DomUtil.create('div', 'info legend'),
+                    grades = [firstBreak, secondBreak, thirdBreak, fourthBreak, fifthBreak],
+                    labels = [],
+                    from, to;
     
-            // //  ------------------------------------
+                for (var i = 0; i < grades.length; i++) {
+                    from = grades[i];
+                    to = grades[i + 1];
+    
+                    labels.push(
+                        '<i style="background:' + getColor(from + 1) + '"></i> ' +
+                        from + (to ? '&ndash;' + to : '+'));
+                }
+                div.innerHTML = labels.join('<br>');
+                return div;
+            };
+            legend.addTo(map);
         }
+        
 
         function clickedFeature(e) {
             var layer = e.target;
             info.update(layer.feature.properties);
-            
+
             if (outbound.checked) {
                 for (var key in map._layers) {
                     for (var i = 0; i < county_outbound.length; i++) {
                         if (map._layers[key].feature && map._layers[key].feature.properties.Municipio === county_outbound[i][0]) {
-                            // console.log(county_outbound);
-                            console.log(county_outbound[i][1]);
                             arr.push(Number(county_outbound[i][1]));
-                            // console.log(arr);
                             map._layers[key].setStyle({
                                 fillColor: '#FD8D3C',
                                 weight: 1,
@@ -177,7 +226,6 @@ const prettyLink  = {
                         }
                     }
                     layer.setStyle({
-                        // fillColor: '#99ccff',
                         weight: 3,
                         color: '#666',
                         dashArray: '',
@@ -185,17 +233,13 @@ const prettyLink  = {
                     });
 
                 }
-                console.log(arr)
                 setJenks(arr);
             }
             else if (inbound.checked) {
                 for (var key in map._layers) {
                     for (var i = 0; i < county_inbound.length; i++) {
                         if (map._layers[key].feature && map._layers[key].feature.properties.Municipio === county_inbound[i][0]) {
-                            console.log(county_inbound);
-                            // console.log(county_inbound[i][1]);
                             arr.push(Number(county_inbound[i][1]));
-                            console.log(arr);
                             map._layers[key].setStyle({
                                 fillColor: '#FC4E2A',
                                 weight: 1,
@@ -207,14 +251,12 @@ const prettyLink  = {
                         }
                     }
                     layer.setStyle({
-                        // fillColor: '#99ccff',
                         weight: 3,
                         color: '#666',
                         dashArray: '',
                         fillOpacity: 0.7
                     });
                 }
-                console.log(arr);
                 setJenks(arr);
             }
             if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
@@ -239,41 +281,6 @@ const prettyLink  = {
         }).addTo(map);
 
         map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census</a>');
-
-        function getColor(d) {
-            return d > 100000 ? '#331a00' :
-                d > 35000 ? '#663300' :
-                    d > 7500 ? '#994d00' :
-                        d > 5000 ? '#cc6600' :
-                            d > 2500 ? '#ff8000' :
-                                d > 1000 ? '#ff9933' :
-                                    d > 500 ? '#e6e6ff' :
-                                        d > 250 ? '#ccccff' :
-                                            d > 100 ? '#9999ff' :
-                                                d > 10 ? '#4d4dff' :
-                                                    '#000080';
-        }
-
-        var legend = L.control({ position: 'bottomright' });
-        legend.onAdd = function (map) {
-
-            var div = L.DomUtil.create('div', 'info legend'),
-                grades = [0, 10, 100, 250, 500, 1000, 2500, 5000, 7500, 10000, 35000],
-                labels = [],
-                from, to;
-
-            for (var i = 0; i < grades.length; i++) {
-                from = grades[i];
-                to = grades[i + 1];
-
-                labels.push(
-                    '<i style="background:' + getColor(from + 1) + '"></i> ' +
-                    from + (to ? '&ndash;' + to : '+'));
-            }
-            div.innerHTML = labels.join('<br>');
-            return div;
-        };
-        legend.addTo(map);
     }
 
     handleChangeMunicipio(event, index, value) {
@@ -283,7 +290,7 @@ const prettyLink  = {
     handleOptionChange(changeEvent) {
         this.setState({ selectedOption: changeEvent.target.value });
     }
-    
+
 
     render() {
         return (
@@ -327,7 +334,7 @@ const prettyLink  = {
                                 <CSVLink data={csvData()} style={prettyLink} filename={"map-data.csv"}>Download Map CSV ⬇ </CSVLink>
                             </div>
                             <div className={'csv-link'}>
-                                <CSVLink data={dataTest} style={prettyLink} filename={"county-data.csv"}>Download Selected County CSV ⬇</CSVLink>
+                                <CSVLink data={csvData()} headers={county_csv_headers} style={prettyLink} filename={"county-data.csv"}>Download Selected County CSV ⬇</CSVLink>
                             </div>
                         </div>
                     </div>
